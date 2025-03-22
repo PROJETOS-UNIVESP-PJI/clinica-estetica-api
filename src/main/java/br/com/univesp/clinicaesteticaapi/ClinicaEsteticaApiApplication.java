66e9bc42f192cccbd06f54c4bc50d8f451003841
douglasdmv <douglasmviegas@gmail.com>
2025-03-22 17:25:05 -0300

    initial commit

---
+++ src/main/java/br/com/univesp/clinicaesteticaapi/ClinicaEsteticaApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.univesp.clinicaesteticaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicaEsteticaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicaEsteticaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/univesp/clinicaesteticaapi/ClinicaEsteticaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.univesp.clinicaesteticaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicaEsteticaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
